9ab7b33690aa8ce9c4a127c2cd545574125968a2
cyf-gh <cyf-ms@hotmail.com>
2019-12-25 23:10:43 +0800

    Implement sync features and lobby features

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
+
 import (
-	"fmt"
-	"net"
-	"time"
+	"runtime"
+	stlog "stgogo/comn/log"
+	"strconv"
 )
 
-func initUdpServer() (*UDPConn, error) {
-	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8800")
-	if err != nil {
-		fmt.Println("ResolveUDPAddr err:", err)
-		return nil, err
-	}
-	conn, err := net.ListenUDP("udp", udpAddr) //创建数据通信socket
-	if err != nil {
-		fmt.Println("ListenUDP err:", err)
-		return nil, err
-	}
-	defer conn.Close()
-	return conn, err
+func initGlobal() {
+	stlog.Info.Println("Server Started...")
+	stlog.Info.Println( "goroutine Run with Core Count: " + strconv.Itoa(runtime.GOMAXPROCS(runtime.NumCPU())))
 }
 
 func main() {
-	fmt.Println("Server Start...")
-	conn, _ := initUdpServer()
-
-	for i := 0; i < 100000000; i++ {
-		time.Sleep(1)
-		buf := make([]byte, 1024)
-		n, raddr, err := conn.ReadFromUDP(buf)
-		if err != nil {
-			return
-		}
-		fmt.Println("[Client]", string(buf[:n]))
-
-		_, err = conn.WriteToUDP([]byte("OK"), raddr)
-		if err != nil {
-			fmt.Println("WriteToUDP err:", err)
-			return
-		}
-	}
+	initGlobal()
 }
---
+++ sync/server.go
@@ -0,0 +1,92 @@
+package vt_sync
+
+import (
+	vtlobby "../lobby"
+	"net"
+	stlog "stgogo/comn/log"
+	stmath "stgogo/comn/math"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// initialize udp connect object
+func Init() (*net.UDPConn, error) {
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8800")
+	if err != nil {
+		stlog.Error.Println("ResolveUDPAddr err:", err)
+		return nil, err
+	}
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		stlog.Error.Println("ListenUDP err:", err)
+		return nil, err
+	}
+	return conn, err
+}
+
+// main process of video sync
+func Proc(conn *net.UDPConn, duration time.Duration, lby *vtlobby.VTLobby) {
+	for {
+		time.Sleep(duration)
+
+		buf := make([]byte, 1024)
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			stlog.Error.Println( err )
+			return
+		}
+		recvUdpMsg := string(buf[:n])
+		stlog.Info.Println("[Client]", recvUdpMsg)
+
+		_, err = conn.WriteToUDP( []byte( checkLocationAndReturn(recvUdpMsg, lby) ), addr,)
+		if err != nil {
+			stlog.Error.Println( err )
+			return
+		}
+	}
+}
+
+func checkLocationAndReturn( udpMsg string, lby *vtlobby.VTLobby ) string {
+	curName, curLocation, isPause := splitNameAndLocationAndIsPauseFlag( udpMsg )
+	pHostViewer := vtlobby.GetHostViewer(lby.Viewers)
+
+	for i, viewer := range lby.Viewers {
+		if viewer.Name == curName {
+			// update location and pause info first
+			lby.Viewers[i].Location = curLocation
+			lby.Viewers[i].IsPause = isPause == "p"
+			// host viewer is always OK
+			if viewer.IsHost { return "OK" }
+
+			if pHostViewer.IsPause != lby.Viewers[i].IsPause {
+				if pHostViewer.IsPause { return "p" } else { return "s" }
+			}
+			hm, hs := splitMinusAndSecond( pHostViewer.Location )
+			cm, cs := splitMinusAndSecond( curLocation )
+			offset := ( hm * 60 + hs ) - ( cm * 60 + cs )
+			offset = stmath.Abs( offset )
+
+			if offset >= lby.MaxOffset  {
+				return pHostViewer.Location
+			}
+		}
+	}
+	return "OK"
+}
+
+func splitMinusAndSecond( currentTime string ) (int, int) {
+	mAnds := strings.Split( currentTime, ":" )
+	m, _ := strconv.Atoi( mAnds[0] )
+	s, _ := strconv.Atoi( mAnds[1] )
+	return m, s
+}
+
+func splitNameAndLocationAndIsPauseFlag( udpMsg string ) ( string, string, string ) {
+	nAl := strings.Split(udpMsg, "," )
+	if len(nameAndLocation) == 1 {
+		stlog.Error.Println("Invalid UDP Message")
+		return "", "", ""
+	}
+	return nAl[0], nAl[1], nAl[2]
+}
