539a7a47d7aa0644b89162d8e61c3a79889dcc38
cyf <cyf-ms@hotmail.com>
2020-01-21 18:11:08 +0800

    Update install dependency file

---
+++ install_dep.sh
@@ -0,0 +1,7 @@
+go get -u gopkg.in/ini.v1
+go get -u github.com/go-playground/assert
+go get -u github.com/kpango/glg
+
+cd $GOPATH/src
+git clone https://github.com/cyf-gh/stgogo.git
+git clone https://github.com/cyf-gh/GoYPM.git
--- install_dependency.ps1
+++
@@ -1,3 +0,0 @@
-go get -u gopkg.in/ini.v1
-go get -u github.com/go-playground/assert
-go get -u github.com/kpango/glg
