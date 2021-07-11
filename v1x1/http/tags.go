67aca8a9493a7f17f299f13c5ce22012ff9ceb8f
cyf <cyf-ms@hotmail.com>
2021-07-11 17:39:24 +0800

    添加注释

--- v1x1/http/tags.go
+++ v1x1/http/tags.go
@@ -8,7 +8,8 @@ import (
 
 func init() {
 	cc.AddActionGroup( "/v1x1/tags", func( a cc.ActionGroup ) error {
-
+		// \brief 获取所有的标签
+		// \note 无
 		a.GET( "", func( ap cc.ActionPackage ) ( cc.HttpErrReturn, cc.StatusCode ) {
 			tags, e := orm.GetAllTags(); err.Assert( e )
 			return cc.HerOkWithData( tags )
